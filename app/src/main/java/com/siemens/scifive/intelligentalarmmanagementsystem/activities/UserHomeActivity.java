06415a601a9f607f429f438473a318c6580072c8
Nishant Kashiv <nishantkashiv@gmail.com>
2018-07-12 20:57:56 +0530

    Added location updates code...

--- app/src/main/java/com/siemens/scifive/intelligentalarmmanagementsystem/activities/UserHomeActivity.java
+++ app/src/main/java/com/siemens/scifive/intelligentalarmmanagementsystem/activities/UserHomeActivity.java
@@ -1,18 +1,47 @@
 package com.siemens.scifive.intelligentalarmmanagementsystem.activities;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.FrameLayout;
 import android.widget.Toast;
 
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+//import com.siemens.scifive.intelligentalarmmanagementsystem.Manifest;
 import com.siemens.scifive.intelligentalarmmanagementsystem.R;
 import com.siemens.scifive.intelligentalarmmanagementsystem.fragments.AlarmDetailsFragment;
 import com.siemens.scifive.intelligentalarmmanagementsystem.fragments.HistoryFragment;
 import com.siemens.scifive.intelligentalarmmanagementsystem.fragments.UserHomeFragment;
 import com.siemens.scifive.intelligentalarmmanagementsystem.fragments.UserLocationFragment;
+import com.siemens.scifive.intelligentalarmmanagementsystem.interfaces.CallMethods;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 public class UserHomeActivity extends AppCompatActivity {
     private static UserHomeActivity INSTANCE = null;
@@ -20,6 +49,19 @@ public class UserHomeActivity extends AppCompatActivity {
     TabLayout tLAUHTabs;
     FragmentManager fragmentManager;
 
+    private CallMethods listener ;
+
+    private static final long UPDATE_INTERVAL_IN_MILLISECONDS = 10000;
+    private static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS =
+            UPDATE_INTERVAL_IN_MILLISECONDS / 2;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private LocationSettingsRequest mLocationSettingsRequest;
+    private LocationRequest mLocationRequest;
+    private LocationCallback mLocationCallback;
+    public Location mCurrentLocation;
+    private Boolean mRequestingLocationUpdates;
+    private String mLastUpdateTime;
+    private SettingsClient mSettingsClient;
     public static UserHomeActivity getInstance() {
         return INSTANCE;
     }
@@ -30,6 +72,15 @@ public class UserHomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_user_home);
         INSTANCE = this;
 
+        mRequestingLocationUpdates = false;
+        mLastUpdateTime = "";
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mSettingsClient = LocationServices.getSettingsClient(this);
+
+        // Kick off the process of building the LocationCallback, LocationRequest, and
+        // LocationSettingsRequest objects.
+
         fLAUHFragmentContainer = findViewById(R.id.fLAUHFragmentContainer);
         tLAUHTabs = findViewById(R.id.tLAUHTabs);
 
@@ -38,6 +89,10 @@ public class UserHomeActivity extends AppCompatActivity {
         setupTabs();
 
         changeFragment(UserHomeFragment.getInstance(), true);
+        createLocationCallback();
+        createLocationRequest();
+        buildLocationSettingsRequest();
+        startLocationUpdates();
     }
 
     private void changeFragment(Fragment fragment, boolean isFirst) {
@@ -119,4 +174,108 @@ public class UserHomeActivity extends AppCompatActivity {
         Toast.makeText(UserHomeActivity.this, text, Toast.LENGTH_SHORT).show();
     }
 
+    private void buildLocationSettingsRequest() {
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(mLocationRequest);
+        mLocationSettingsRequest = builder.build();
+    }
+
+    private void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+
+        // Sets the desired interval for active location updates. This interval is
+        // inexact. You may not receive updates at all if no location sources are available, or
+        // you may receive them slower than requested. You may also receive updates faster than
+        // requested if other applications are requesting location at a faster interval.
+        mLocationRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
+
+        // Sets the fastest rate for active location updates. This interval is exact, and your
+        // application will never receive updates faster than this value.
+        mLocationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS);
+
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    private void createLocationCallback() {
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+
+                mCurrentLocation = locationResult.getLastLocation();
+                //listener.setCurrentLocation(mCurrentLocation);
+                Log.d("Location", "Location update recieved");
+                //listener.setAlarmLocation();
+                //Fragment locFrag = UserLocationFragment.getInstance();
+                //locFrag.
+                mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+                //updateLocationUI();
+            }
+        };
+    }
+
+    public void setListener(CallMethods listener)
+    {
+        this.listener = listener ;
+    }
+
+    private void startLocationUpdates() {
+        // Begin by checking if the device has the necessary location settings.
+        mSettingsClient.checkLocationSettings(mLocationSettingsRequest)
+                .addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+                    @Override
+                    public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                        Log.i("Location", "All location settings are satisfied.");
+
+                        //noinspection MissingPermission
+
+                        if (ActivityCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                                != PackageManager.PERMISSION_GRANTED) {
+                            // Check Permissions Now
+                            ActivityCompat.requestPermissions(UserHomeActivity.this,
+                                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                    99);
+                        } else {
+                            // permission has been granted, continue as usual
+                            //Location myLocation =
+                            //        LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+                            mFusedLocationClient.requestLocationUpdates(mLocationRequest,
+                                    mLocationCallback, Looper.myLooper());
+                        }
+
+                        //mFusedLocationClient.requestLocationUpdates(mLocationRequest,
+                        //        mLocationCallback, Looper.myLooper());
+
+                        //updateUI();
+                    }
+                })
+                .addOnFailureListener(this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        int statusCode = ((ApiException) e).getStatusCode();
+                        switch (statusCode) {
+                            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                                Log.i("Location", "Location settings are not satisfied. Attempting to upgrade " +
+                                        "location settings ");
+                                try {
+                                    // Show the dialog by calling startResolutionForResult(), and check the
+                                    // result in onActivityResult().
+                                    ResolvableApiException rae = (ResolvableApiException) e;
+                                    rae.startResolutionForResult(UserHomeActivity.this, 99);
+                                } catch (IntentSender.SendIntentException sie) {
+                                    Log.i("Location", "PendingIntent unable to execute request.");
+                                }
+                                break;
+                            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                                String errorMessage = "Location settings are inadequate, and cannot be " +
+                                        "fixed here. Fix in Settings.";
+                                Log.e("Location", errorMessage);
+                                Toast.makeText(UserHomeActivity.this, errorMessage, Toast.LENGTH_LONG).show();
+                                mRequestingLocationUpdates = false;
+                        }
+
+                        //updateUI();
+                    }
+                });
+    }
 }
--- app/src/main/java/com/siemens/scifive/intelligentalarmmanagementsystem/fragments/UserLocationFragment.java
+++ app/src/main/java/com/siemens/scifive/intelligentalarmmanagementsystem/fragments/UserLocationFragment.java
@@ -1,25 +1,51 @@
 package com.siemens.scifive.intelligentalarmmanagementsystem.fragments;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 import com.siemens.scifive.intelligentalarmmanagementsystem.R;
 import com.siemens.scifive.intelligentalarmmanagementsystem.activities.UserHomeActivity;
+import com.siemens.scifive.intelligentalarmmanagementsystem.interfaces.CallMethods;
 
-public class UserLocationFragment extends Fragment {
+public class UserLocationFragment extends Fragment implements CallMethods {
     private static UserLocationFragment INSTANCE = null;
     SupportMapFragment supportMapFragment;
     GoogleMap map;
+
+    Location currentLocation;
+    Location alarmLocation;
+    private boolean alarmLocationRecieved = false;
+    private boolean currentLocationRecieved = false;
+    public void setAlarmLocation(Location alarmLocation){
+        this.alarmLocation = alarmLocation;
+        alarmLocationRecieved = true;
+        Log.d("Location","alarm Location recieved");
+    }
+
+    public void setCurrentLocation(Location currentLocation){
+        this.currentLocation = currentLocation;
+        currentLocationRecieved = true;
+        Log.d("Location","current Location recieved");
+    }
 
     public static UserLocationFragment getInstance() {
         if (INSTANCE == null) {
@@ -41,9 +67,38 @@ public class UserLocationFragment extends Fragment {
             public void onMapReady(GoogleMap googleMap) {
                 map = googleMap;
                 map.setMyLocationEnabled(true);
+                //if(currentLocationRecieved){
+                currentLocation = UserHomeActivity.getInstance().mCurrentLocation;
+                LatLng currLatLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+                map.moveCamera(CameraUpdateFactory.newLatLng(currLatLng));
+            //}
+                //map.moveCamera();
             }
         });
         return v;
     }
+
+    public View onResume(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        View v = inflater.inflate(R.layout.fragment_user_location, container, false);
+        supportMapFragment = (SupportMapFragment) this.getChildFragmentManager()
+                .findFragmentById(R.id.map);
+
+        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+            @SuppressLint("MissingPermission")
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                map = googleMap;
+                map.setMyLocationEnabled(true);
+                //if(currentLocationRecieved){
+                currentLocation = UserHomeActivity.getInstance().mCurrentLocation;
+                LatLng currLatLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+                map.moveCamera(CameraUpdateFactory.newLatLng(currLatLng));
+                //}
+                //map.moveCamera();
+            }
+        });
+        return v;
+    }
+
 
 }
